1561e85e3f6e19be1ce795d8e3cd0c68cc821607
Boccard Pierre-alexis <uapv1600460@adm-2w22a-alex.univ-avignon.fr>
2015-11-18 17:29:31 +0100

    layout couleur

--- OurFace/view/layout.php
+++ OurFace/view/layout.php
@@ -46,74 +46,74 @@
 		</nav>
 	</header>
 
-<div class="container-fluid">
-	<div id="corps"  class="row">
-		<div id="listeAmi" class="col-xs-offset-10 col-md-offset-10">
-			<div id="friend" class="row friends">
-				<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
-				<span class="friendName">Araud quentin</span>
-			</div>
-			<div id="friend2" class="row friends">
-				<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
-				<span class="friendName">Pierre-Alexis Boccard</span>
-			</div>
-		</div>
-
-	</div>
+	<div class="container-fluid">
+		<div  class="row">
 
 
 
-			<!-- *****************  MUR Pierre-alexis Boccard  ******** -->
-		<div class="row area">
-			<div class="col-md-offset-2 col-md-4">
-				<form role="form" >
-					<div class="form-group">
-						<textarea class="form-control" rows="5">Entrez votre message</textarea>
-						<div class="col-md-offset-6 col-md-6 cancel-padding">
-							<div class="btn-group btn-group-justified" role="group">
-								<div class="btn-group">
-									<button class="btn btn-default btn-success" type="submit"><span class="glyphicon glyphicon-file"></span> Joindre</button>
+
+				<!-- *****************  MUR Pierre-alexis Boccard  ******** -->
+			<div class="col-md-10">
+				<div class="row area">
+					<div class="col-md-offset-4 col-md-6">
+						<form role="form" >
+							<div class="form-group">
+								<textarea class="form-control" rows="5">Entrez votre message</textarea>
+								<div class="col-md-offset-5 col-md-7 cancel-padding">
+									<div class="btn-group btn-group-justified" role="group">
+										<div class="btn-group">
+											<button class="btn btn-default btn-warning" type="submit"><span class="glyphicon glyphicon-file"></span> Joindre</button>
+										</div>
+					                    <div class="btn-group">
+					                		<button class="btn btn-default btn-primary" type="submit"><i class="">Publier</i></button>
+					                 	</div>
+					                </div>
 								</div>
-			                    <div class="btn-group">
-			                		<button class="btn btn-default btn-primary" type="submit"><i class="">Publier</i></button>
-			                 	</div>
-			                </div>
-						</div>
-					</div>
-				</form>	
-	        </div>	
-		</div>
+							</div>
+						</form>	
+			        </div>	
+				</div>
 
-		<div class="row area">
-			<div class="col-md-offset-2 col-md-4">
-				<form role="form" >
-					<div class="form-group">
-						<div>
-							<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
-							<span class="friendName">Araud quentin</span>
-						</div>
-						<textarea class="form-control" rows="5">J'ai une tête de yonf</textarea>
-						<div class="col-md-offset-6 col-md-6 cancel-padding">
-							<div class="btn-group btn-group-justified" role="group">
-								<div class="btn-group">
-									<button class="btn btn-default btn-success" type="submit"><span class="glyphicon glyphicon-file"></span> Joindre</button>
+
+				<div class="row area">
+					<div class="col-md-offset-4 col-md-6">
+						<form role="form" >
+							<div class="form-group">
+								<div>
+									<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
+									<span class="friendName">Araud quentin</span>
 								</div>
-			                    <div class="btn-group">
-			                		<button class="btn btn-default btn-primary" type="submit"><i class="">Publier</i></button>
-			                 	</div>
-			                </div>
-						</div>
+								<textarea readonly class="form-control" rows="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ante mauris. Sed auctor, dui eu semper sodales, eros massa accumsan magna, id fermentum elit nibh.</textarea>
+								<div class="col-md-offset-5 col-md-7 cancel-padding">
+									<div class="btn-group btn-group-justified" role="group">
+										<div class="btn-group">
+											<button class="btn btn-default btn-success" type="submit"><span class="glyphicon glyphicon-thumbs-up"></span> J'aime</button>
+										</div>
+					                    <div class="btn-group">
+					                		<button class="btn btn-default btn-primary" type="submit"><i class="">Répondre</i></button>
+					                 	</div>
+					          	     </div>
+								</div>
+							</div>
+						</form>	
+			        </div>	
+				</div>
+			</div>
+
+			<div id="corps" class="col-md-2 cancel-padding">
+				<div id="listeAmi" class="">
+					<div id="friend" class="row friends">
+						<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
+						<span class="friendName">Araud quentin</span>
 					</div>
-				</form>	
-	        </div>	
+					<div id="friend2" class="row friends">
+						<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
+						<span class="friendName">Pierre-Alexis Boccard</span>
+					</div>
+				</div>
+			</div>
 		</div>
-
 	</div>
-
-
-
-
-
 
 	<footer>
 	</footer>
