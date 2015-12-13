e8d857496278b2f8b2298e2e09e7efa0d4837423
yewed <pa.boccard@gmail.com>
2015-12-13 01:07:59 +0100

    commit css & change design of wall

--- OurFace/view/wallSuccess.php
+++ OurFace/view/wallSuccess.php
@@ -1,18 +1,21 @@
 <div class="">
 	<div class="row area">
-		<div class="col-lg-offset-4 col-lg-6 col-md-offset-3 col-md-8">
+		<div class="col-lg-offset-4 col-lg-7 col-md-offset-3 col-md-9">
 			<form role="form" >
 				<div class="form-group">
-					<textarea class="form-control" rows="5">Exprimez-vous...</textarea>
-					<div class="col-lg-offset-5 col-lg-7 cancel-padding">
-						<div class="btn-group btn-group-justified" role="group">
-							<div class="btn-group">
-								<button class="btn btn-default btn-warning" type="submit"><span class="glyphicon glyphicon-file"></span> Joindre</button>
+					<div class="box">
+					    <textarea id="status" class"form-control" rows="5">Exprimez-vous...</textarea>
+					    <hr/>
+							<div class="btn-group pull-left" role="group">
+								<div class="btn-group">
+									<button class="btn btn-msg msg-color-insert" type="submit"><span class="glyphicon glyphicon-file"></span> Joindre</button>
+								</div>
 							</div>
-		                    <div class="btn-group">
-		                		<button class="btn btn-default btn-primary" type="submit">Publier</button>
-		                 	</div>
-		                </div>
+							<div class="btn-group pull-right" role="group">
+		                    	<div class="btn-group">
+		                			<button class="btn btn-msg msg-color-com" type="submit">Publier</button>
+		                 		</div>
+					    	</div>
 					</div>
 				</div>
 			</form>	
@@ -26,28 +29,31 @@
 
 	?> 
 	<div class="row area">
-		<div class="col-lg-offset-4 col-lg-6 col-md-offset-3 col-md-8">
+		<div class="col-lg-offset-4 col-lg-7 col-md-offset-3 col-md-9">
 			<form role="form" >
-			<div class="form-group">
-				<div>
-					<img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
-					<span id="friend" class="friendName">
-						<?php echo ("<a href=\"OurFace.php?action=myWall&profile=".utilisateurTable::getUserById($key->getEmetteur())->getIdentifiant()."\">");
-						echo utilisateurTable::getUserById($key->getEmetteur())->getIdentifiant() ; 
-						echo ('</a>');
-						?>
-					</span>
-				</div>
-				<textarea readonly class="form-control" rows="5"><?php echo postTable::getPostById($key->getPost())->getTexte() ;?></textarea>
-					<div class="col-lg-offset-5 col-lg-7 cancel-padding">
-						<div class="btn-group btn-group-justified" role="group">
-							<div class="btn-group">
-								<button class="btn btn-default btn-success" type="submit"><span class="glyphicon glyphicon-thumbs-up"></span> J'aime</button>
+				<div class="form-group">
+					<div class="box">
+						<div id="friend" class="friends">
+						    <img src="images/test.jpg" alt="oui" class="img-rounded friendPicture">
+						    <span id="friend" class="friendName">
+								<?php echo ("<a href=\"OurFace.php?action=myWall&profile=".utilisateurTable::getUserById($key->getEmetteur())->getIdentifiant()."\">");
+								echo utilisateurTable::getUserById($key->getEmetteur())->getIdentifiant() ; 
+								echo ('</a>');
+								?>
+							</span>
+						</div>
+					    <textarea readonly id="status" class"form-control" rows="0"><?php echo postTable::getPostById($key->getPost())->getTexte() ;?></textarea>
+					    <hr/>
+							<div class="btn-group pull-left" role="group">
+								<div class="btn-group">
+									<button class="btn btn-msg msg-color-like" type="submit"><span class="glyphicon glyphicon-thumbs-up"></span> J'aime <?php echo $key->getAime(); ?></button>
+								</div>
 							</div>
-                   			<div class="btn-group">
-               					<button class="btn btn-default btn-primary" type="submit">Répondre</button>
-               				</div>
-    	     			</div>
+							<div class="btn-group pull-right" role="group">
+	                   			<div class="btn-group">
+	               					<button class="btn btn-msg msg-color-com" type="submit">Commenter</button>
+	               				</div>
+					    	</div>
 					</div>
 				</div>
 			</form>
